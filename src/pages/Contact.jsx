9735b02a664a550f997c88339f324a10ff36cb42
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-12-11 15:13:18 +0600

    create contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,13 @@
-// import Contactdiv from "../componets/Contactdiv";
-// import EmailForm from "../componets/EmailForm";
+import Contactdiv from "../componets/Contactdiv";
+import EmailForm from "../componets/EmailForm";
 
-// const Contact = () => {
-//     return (
-//         <div>
-//             <Contactdiv></Contactdiv>
-//             <EmailForm></EmailForm>
-//         </div>
-//     );
-// };
+const Contact = () => {
+    return (
+        <div>
+            <Contactdiv></Contactdiv>
+            <EmailForm></EmailForm>
+        </div>
+    );
+};
 
-// export default Contact;
+export default Contact;
