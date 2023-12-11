15bf0751eb87b75e19223dae8200a2032aa4a91b
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-12-11 15:16:37 +0600

    add aducational information

--- src/componets/Aducation.jsx
+++ src/componets/Aducation.jsx
@@ -1,50 +1,50 @@
 
-// const Aducation = () => {
-//     return (
-//         <div className="my-5">
-//             <h1 className=" py-10 text-4xl font-bold underline text-blue-500 text-center">Aducation</h1>
-//             <div>
-//                 <table className="border w-full text-center">
-//                     <thead className="">
-//                         <tr className="border border-2 border-blue-500">
-//                             <td className="border border-blue-500">Institute Name</td>
-//                             <td className="border border-blue-500">Examination</td>
-//                             <td className="border border-blue-500">Group</td>
-//                             <td className="border border-blue-500">Exam Board</td>
-//                             <td className="border border-blue-500">G.P.A</td>
-//                             <td className="border border-blue-500">Passing Year</td>
-//                         </tr>
-//                     </thead>
-//                     <tbody>
-//                         <tr>
-//                             <td className="border border-blue-500">Sataish School and College</td>
-//                             <td className="border border-blue-500">S.S.C</td>
-//                             <td className="border border-blue-500">Science</td>
-//                             <td className="border border-blue-500">Dhaka</td>
-//                             <td className="border border-blue-500">4.00</td>
-//                             <td className="border border-blue-500">2020</td>
-//                         </tr>
-//                         <tr>
-//                             <td className="border border-blue-500">Durgapur Govt. College</td>
-//                             <td className="border border-blue-500">H.S.C</td>
-//                             <td className="border border-blue-500">Science</td>
-//                             <td className="border border-blue-500">Maymensing</td>
-//                             <td className="border border-blue-500">4.17</td>
-//                             <td className="border border-blue-500">2023</td>
-//                         </tr>
-//                         <tr>
-//                             <td className="border border-blue-500">Madaripur Govt. College</td>
-//                             <td className="border border-blue-500">B.Sc</td>
-//                             <td className="border border-blue-500">Mathmatics</td>
-//                             <td className="border border-blue-500">Borisal</td>
-//                             <td className="border border-blue-500">continue</td>
-//                             <td className="border border-blue-500">continue</td>
-//                         </tr>
-//                     </tbody>
-//                 </table>
-//             </div>
-//         </div>
-//     );
-// };
+const Aducation = () => {
+    return (
+        <div className="my-5">
+            <h1 className=" py-10 text-4xl font-bold underline text-blue-500 text-center">Aducation</h1>
+            <div>
+                <table className="border w-full text-center">
+                    <thead className="">
+                        <tr className="border border-2 border-blue-500">
+                            <td className="border border-blue-500">Institute Name</td>
+                            <td className="border border-blue-500">Examination</td>
+                            <td className="border border-blue-500">Group</td>
+                            <td className="border border-blue-500">Exam Board</td>
+                            <td className="border border-blue-500">G.P.A</td>
+                            <td className="border border-blue-500">Passing Year</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="border border-blue-500">Sataish School and College</td>
+                            <td className="border border-blue-500">S.S.C</td>
+                            <td className="border border-blue-500">Science</td>
+                            <td className="border border-blue-500">Dhaka</td>
+                            <td className="border border-blue-500">4.00</td>
+                            <td className="border border-blue-500">2020</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-blue-500">Durgapur Govt. College</td>
+                            <td className="border border-blue-500">H.S.C</td>
+                            <td className="border border-blue-500">Science</td>
+                            <td className="border border-blue-500">Maymensing</td>
+                            <td className="border border-blue-500">4.17</td>
+                            <td className="border border-blue-500">2023</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-blue-500">Madaripur Govt. College</td>
+                            <td className="border border-blue-500">B.Sc</td>
+                            <td className="border border-blue-500">Mathmatics</td>
+                            <td className="border border-blue-500">Borisal</td>
+                            <td className="border border-blue-500">continue</td>
+                            <td className="border border-blue-500">continue</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
 
-// export default Aducation;
+export default Aducation;
