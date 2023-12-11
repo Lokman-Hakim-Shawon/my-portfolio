531829f6a6c5dfac4352d2e009753889bb28a89e
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-12-11 15:06:22 +0600

    set my letest project

--- src/componets/MyProjects.jsx
+++ src/componets/MyProjects.jsx
@@ -1,63 +1,63 @@
-// import { Link } from 'react-router-dom';
-// import img1 from '../assets/images/project1.png'
-// import img2 from '../assets/images/project2.png'
-// import img3 from '../assets/images/project3.png'
-// const MyProjects = () => {
-//     return (
-//         // text div
-//         <div>
-//             <div className="lg:my-10">
-//             <hr className="w-1/2 mx-auto border border-blue-300 md:border-2 lg:border-4 rounded-xl"/>
-//             <h1 className="text-center md:text-2xl lg:text-4xl font-bold md:py-2 lg:py-5 text-blue-500">My Letest Project</h1>
-//             <hr className="w-1/2 mx-auto border border-blue-300 md:border-2 lg:border-4 rounded-xl"/>
-//            </div>
+import { Link } from 'react-router-dom';
+import img1 from '../assets/images/project1.png'
+import img2 from '../assets/images/project2.png'
+import img3 from '../assets/images/project3.png'
+const MyProjects = () => {
+    return (
+        // text div
+        <div>
+            <div className="lg:my-10">
+            <hr className="w-1/2 mx-auto border border-blue-300 md:border-2 lg:border-4 rounded-xl"/>
+            <h1 className="text-center md:text-2xl lg:text-4xl font-bold md:py-2 lg:py-5 text-blue-500">My Letest Project</h1>
+            <hr className="w-1/2 mx-auto border border-blue-300 md:border-2 lg:border-4 rounded-xl"/>
+           </div>
 
-//         {/* letest project div */}
-//         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-5">
-//             {/* 1st project card */}
-//         <div className="card  bg-base-100 border border-blue-500">
-//   <figure className="px-10 pt-10">
-//     <img src={img1} alt="Shoes" className="rounded-xl" />
-//   </figure>
-//   <div className="card-body items-center text-center">
-//     <h2 className="card-title text-blue-500">Blood Donation</h2>
-//     <p>This is a blood donation website. i make it for donor so that they give their blood very easily.</p>
-//     <div className="card-actions">
-//       <Link to='https://assignment-12-ac346.web.app/'><button className="btn bg-blue-500 text-white">View Site</button></Link>
-//     </div>
-//   </div>
-// </div>
-//   {/* 2nd project card */}
-//   <div className="card bg-base-100 border border-blue-500">
-//   <figure className="px-10 pt-10">
-//     <img src={img2} alt="Shoes" className="rounded-xl" />
-//   </figure>
-//   <div className="card-body items-center text-center">
-//     <h2 className="card-title text-blue-500">Blog Site</h2>
-//     <p>I make it for writing blog. Anyone write blog here. Others person comment your blog and you can see those comment.</p>
-//     <div className="card-actions">
-//       <Link to='https://decoded-petal-377806.web.app/'><button className="btn bg-blue-500 text-white ">View site</button></Link>
-//     </div>
-//   </div>
-// </div>
-// {/* 3rd project card */}
-// <div className="card bg-base-100 border border-blue-500">
-//   <figure className="px-10 pt-10">
-//     <img src={img3} alt="Shoes" className="rounded-xl" />
-//   </figure>
-//   <div className="card-body items-center text-center">
-//     <h2 className="card-title text-blue-500">Mobile Shop</h2>
-//     <p>this is a online mobile shop like a e-commerce site. you can sell your phone here and see your products.</p>
-//     <div className="card-actions">
-//       <Link to='https://assignment-10-copy.web.app/'><button className="btn bg-blue-500 text-white">View Site</button></Link>
-//     </div>
-//   </div>
-// </div>
+        {/* letest project div */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-5">
+            {/* 1st project card */}
+        <div className="card  bg-base-100 border border-blue-500">
+  <figure className="px-10 pt-10">
+    <img src={img1} alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title text-blue-500">Blood Donation</h2>
+    <p>This is a blood donation website. i make it for donor so that they give their blood very easily.</p>
+    <div className="card-actions">
+      <Link to='https://assignment-12-ac346.web.app/'><button className="btn bg-blue-500 text-white">View Site</button></Link>
+    </div>
+  </div>
+</div>
+  {/* 2nd project card */}
+  <div className="card bg-base-100 border border-blue-500">
+  <figure className="px-10 pt-10">
+    <img src={img2} alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title text-blue-500">Blog Site</h2>
+    <p>I make it for writing blog. Anyone write blog here. Others person comment your blog and you can see those comment.</p>
+    <div className="card-actions">
+      <Link to='https://decoded-petal-377806.web.app/'><button className="btn bg-blue-500 text-white ">View site</button></Link>
+    </div>
+  </div>
+</div>
+{/* 3rd project card */}
+<div className="card bg-base-100 border border-blue-500">
+  <figure className="px-10 pt-10">
+    <img src={img3} alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title text-blue-500">Mobile Shop</h2>
+    <p>this is a online mobile shop like a e-commerce site. you can sell your phone here and see your products.</p>
+    <div className="card-actions">
+      <Link to='https://assignment-10-copy.web.app/'><button className="btn bg-blue-500 text-white">View Site</button></Link>
+    </div>
+  </div>
+</div>
   
-//         </div>
+        </div>
 
-//         </div>
-//     );
-// };
+        </div>
+    );
+};
 
-// export default MyProjects;
+export default MyProjects;
