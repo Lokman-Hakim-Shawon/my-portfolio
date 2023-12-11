fff7b7e90f92f11a2b192eed75ae40392f171eba
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-12-11 15:14:32 +0600

    update main.js

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -1,15 +1,15 @@
-// import { Outlet } from "react-router-dom";
-// import Navbar from "./componets/Navbar";
-// import Footer from "./componets/Footer";
+import { Outlet } from "react-router-dom";
+import Navbar from "./componets/Navbar";
+import Footer from "./componets/Footer";
 
-// const MainLayout = () => {
-//     return (
-//         <div>
-//             <Navbar></Navbar>
-//             <Outlet></Outlet>
-//             <Footer></Footer>
-//         </div>
-//     );
-// };
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
 
-// export default MainLayout;
+export default MainLayout;
