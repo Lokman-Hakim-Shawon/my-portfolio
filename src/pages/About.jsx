b5bc6a4dcd0e0540cfca7f538d5a98cc4e5f89ff
lokman hakim <lokmanhakimshawon3@gmail.com>
2023-12-11 15:09:02 +0600

    create about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,13 @@
-// import AboutDetails from "../componets/AboutDetails";
-// import Aducation from "../componets/Aducation";
+import AboutDetails from "../componets/AboutDetails";
+import Aducation from "../componets/Aducation";
 
-// const About = () => {
-//     return (
-//         <div>
-//            <AboutDetails></AboutDetails>
-//            <Aducation></Aducation>
-//         </div>
-//     );
-// };
+const About = () => {
+    return (
+        <div>
+           <AboutDetails></AboutDetails>
+           <Aducation></Aducation>
+        </div>
+    );
+};
 
-// export default About;
+export default About;
